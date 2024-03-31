146e2da658e773e42a7d5aed5a15852b7ff32cf2
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-03-31 17:09:13 -0300

    actualización de archivos

--- lang/fr/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    "nombre" => "Nom",
-    "mensaje" => "Message",
-    "asunto" => "Sujet",
-    "indicacion" => "Écrivez un message",
-    "suma" => "Combien font",
-    "sobremi" => "Je suis Leonardo Sales de Salta, Argentine. J'ai commencé dans le monde de la programmation il y a quelques années lorsque j'ai pu développer un script rendant le synthétiseur Serum accessible aux lecteurs d'écran. Cette réussite m'a encouragé à approfondir mes connaissances dans le domaine du développement de logiciels, j'ai donc commencé à suivre plusieurs cours. Actuellement, je me sens le plus à l'aise en programmation avec JavaScript (React, Node) et PHP (Laravel).",
-];
